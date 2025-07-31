549bdc5c7b868c636d21d2a16b188de2e23758c2
Syed Farhan <farhansyed2045@gmail.com>
2025-07-31 23:36:39 +0530

    basic setup

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express=require("express");
 const app=express();
 
 app.get("/home",(reqe,res)=>{
-    res.json({me"Hello world"});
+    res.json({message:"Hello world"});
 })
 
 app.listen(8000,()=>{
